f76d45990ca21374b492eea0b6fcf707dd5792ca
Jonk <jonathan.ko523@gmail.com>
2023-10-01 21:59:59 -0700

    cleanup

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,14 +1,7 @@
-// 1) Import React and ReactDom Libraries
 import React from 'react';
 
 
-// 2) Get a refernce to the div with root
 
-
-// 3) Tell react to take control of that element
-
-
-// 4) Create a component
 
 function MainPage(){
     return (
--- src/components/navigationBar.js
+++ src/components/navigationBar.js
@@ -1,17 +1,7 @@
-// 1) Import React and ReactDom Libraries
 import React from 'react';
 
 
-// 2) Get a refernce to the div with root
-
-
-// 3) Tell react to take control of that element
-
-
-// 4) Create a component
-
 function navBar(){
-    console.log("TEST")
     return (
         <div className="text-3xl font-bold underline border-black col-span-2 row-span-5	">
           Navigations
