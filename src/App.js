b1c93b278ad73c87e5760568dc3c85511e6050e8
Jonk <jonathan.ko523@gmail.com>
2023-10-01 22:04:09 -0700

    oved state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import NavigationBar from "./components/navigationBar.js"
 import MainPage from './components/MainPage.js'
 import Header from './components/Header.js'
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+
+
+  const [age, setAge] = useState(28);
+
   return (
     <div className="App grid grid-cols-8 h-screen	">
         <Header/>
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,11 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 
 
 
 function MainPage(){
 
-  const [age, setAge] = useState(28);
+
+
+
+
+
+
 
     return (
         <div className="text-3xl font-bold underline border-black col-span-6 row-span-5">
