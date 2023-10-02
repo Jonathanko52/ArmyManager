fc023a22ba9244b9144850154e6e82673c8df925
Jonk <jonathan.ko523@gmail.com>
2023-10-01 22:02:58 -0700

    State added

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 
 
 function MainPage(){
+
+  const [age, setAge] = useState(28);
+
     return (
         <div className="text-3xl font-bold underline border-black col-span-6 row-span-5">
           Mainpage
