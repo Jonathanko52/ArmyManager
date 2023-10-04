0197e6c6a88c8ed0f422e0985b61195709b6bb95
Jonk <jonathan.ko523@gmail.com>
2023-10-03 22:10:51 -0700

    left page blueprint added

--- src/components/MainLeft.js
+++ src/components/MainLeft.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+  //pages:Main page 
+  //window displaying army
+    //Show army roster, point cost per unit, increase/decrease unit size, remove unit, duplicate unit
+    //show point cost total
+  //window for adding new unit
+    //submit button
+    //input field for name, model count , point per size, unit sizes,
 function MainLeft(){
 
     return (
         <div className="text-3xl font-bold underline border-black col-span-6 row-span-5 border-2">
+        <form>
+            </form>
           MainLeft
         </div>
       )
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -16,7 +16,7 @@ function MainPage(){
     return (
         <div className="text-3xl font-bold underline border-black col-span-10 grid grid-cols-12 border-2 row-span-5">
           <MainLeft/>
-            <MainRight/>
+          <MainRight/>
         </div>
       )
 }
