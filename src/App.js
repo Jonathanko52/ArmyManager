a900a64f33d91600bb20cb7a911f6f26b60f1543
Jonk <jonathan.ko523@gmail.com>
2023-10-03 21:54:33 -0700

    more outlining

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ function App() {
 
   //full army state object
   //{faction:string, units: object, enhancements:object, pointcost: num, modelcount: num, id: number}
-  //units object: { unit name: string, model count: num, point cost per size: array, unit sizes: array, unit number of sizes: number, id:number}
+  //units object: { unit name: string, model count: array, point cost per size: array, unit sizes: array, id:number}
 
 
   //functions:
   //create new army: Army name
   //delete army: armyId
-  //Add unit to army: { unit name: string, model count: num, point cost per size: array, unit sizes: array, id}
+  //Add unit to army: { unit name: string, model count: array, point cost per size: array, unit sizes: array, id}
   //delete unit from army:{id:number}
   
   //save army: local storage id
@@ -25,11 +25,11 @@ function App() {
 
   //pages:Main page 
   //window displaying army
-    //Show army roster, point cost per unit, increase/decrease unit size
+    //Show army roster, point cost per unit, increase/decrease unit size, remove unit, duplicate unit
     //show point cost total
   //window for adding new unit
     //submit button
-    //input field for name, model count, point per size, unit sizes,
+    //input field for name, model count , point per size, unit sizes,
   
   return (
     <div className="App grid grid-cols-8 h-screen	">
