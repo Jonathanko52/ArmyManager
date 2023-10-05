246c9e95c6b182197f9c6488c979e8ad274e6eab
Jonk <jonathan.ko523@gmail.com>
2023-10-04 23:58:38 -0700

    Unit row component created

--- src/components/MainPage/MainRight.js
+++ src/components/MainPage/MainRight.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import UnitRow from './UnitRow.js'
 
 function MainRight(){
 
     return (
         <div className="font-bold underline border-black col-span-8 row-span-5 border-2">
           MainRight
+          <UnitRow/>
+          <UnitRow/>
+          <UnitRow/>
+          <UnitRow/>
+
         </div>
       )
 }
---
+++ src/components/MainPage/UnitRow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UnitRow(){
+
+    return (
+        <div className="font-bold underline border-black col-span-8 row-span-5 border-2">
+          UNIT ROW
+        </div>
+      )
+}
+
+export default UnitRow;
