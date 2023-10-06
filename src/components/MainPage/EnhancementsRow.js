776d6e85b79595b4389948f282da4c565a5f9e27
Jonk <jonathan.ko523@gmail.com>
2023-10-05 23:28:52 -0700

    Enchancment row added. needs revisiting

---
+++ src/components/MainPage/EnhancementsRow.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function EnhancementRow(){
+
+    return (
+        <div className="font-bold underline border-black col-span-8 row-span-5 border-2">
+        <label>Enhancement Name:</label>
+        <label>Point Cost:</label>
+
+        </div>
+      )
+}
+
+export default EnhancementRow;
--- src/components/MainPage/MainRight.js
+++ src/components/MainPage/MainRight.js
@@ -2,6 +2,7 @@ import React from 'react';
 import UnitRow from './UnitRow.js'
 
 function MainRight(){
+  //{faction:string, units: object, enhancements:object, pointcost: num, modelcount: num, id: number}
 
     return (
         <div className="font-bold underline border-black col-span-8 row-span-5 border-2">
