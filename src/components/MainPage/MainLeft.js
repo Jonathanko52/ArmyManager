d2de7b8acd0b0a871207f39708370df42a9a6e37
Jonk <jonathan.ko523@gmail.com>
2023-10-05 23:33:14 -0700

    formatting

--- src/components/MainPage/MainLeft.js
+++ src/components/MainPage/MainLeft.js
@@ -10,14 +10,14 @@ function MainLeft(){
 
     return (
         <div className="p-2 m-2 border-black col-span-4 row-span-5 border-1">
-        <h3>MainLeft</h3>
-        <form>
-            <label>Name</label><br/><input id="MainLeftName"></input><br/>
-            <label>Model Count</label><br/><input id="MainLeftModel"></input><br/>
-            <label>Point Cost</label><br/><input id="MainLeftPoint"></input><br/>
-            <label>Unit sizes</label><br/><input id="MainLeftSizes"></input><br/>
-            <input className = "rounded-md p-2 mt-2 bg-slate-50" type="submit" value="Add Unit"></input>
-        </form>
+          <h3>Add unit</h3>
+          <form>
+              <label>Name</label><br/><input id="MainLeftName"></input><br/>
+              <label>Model Count</label><br/><input id="MainLeftModel"></input><br/>
+              <label>Point Cost</label><br/><input id="MainLeftPoint"></input><br/>
+              <label>Unit sizes</label><br/><input id="MainLeftSizes"></input><br/>
+              <input className = "rounded-md p-2 mt-2 bg-slate-50" type="submit" value="Add Unit"></input>
+          </form>
         </div>
       )
 }
--- src/components/MainPage/UnitRow.js
+++ src/components/MainPage/UnitRow.js
@@ -4,13 +4,12 @@ function UnitRow(){
 
     return (
         <div className="font-bold underline border-black col-span-8 row-span-5 border-2">
-        <label>Unit Name:</label>
-        <label>Size:</label>
-        <label>Point Cost:</label>
-        <label>Enhancements:</label>
-        <button>Increase size</button>
-        <button>Decrease Size</button>
-
+          <label>Unit Name:</label>
+          <label>Size:</label>
+          <label>Point Cost:</label>
+          <label>Enhancements:</label>
+          <button>Increase size</button>
+          <button>Decrease Size</button>
         </div>
       )
 }
