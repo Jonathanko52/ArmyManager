dd427b393474113f96f6b8c1480b3d4c593987b2
Jonk <jonathan.ko523@gmail.com>
2023-10-07 22:05:10 -0700

    css improvements

--- src/components/MainPage/MainRight.js
+++ src/components/MainPage/MainRight.js
@@ -3,27 +3,31 @@ import UnitRow from './UnitRow.js'
 
 function MainRight({army}){
   //{faction:string, units: object, enhancements:object, pointcost: num, modelcount: num, id: number}
+  console.log(army)
   let units = [];
   // useEffect(() =>{
     army.units.forEach(cur=>{
       units.push(<UnitRow unitName={cur.unitName} modelCount={cur.modelCount} pointCost={cur.pointCost} unitSize={cur.unitSize}/>)
     })
 
-  // },[])
+  // },[army])
 
 
 
     return (
         <div className="font-bold underline border-black col-span-8 row-span-5 border-2">
-          <div className = "m-8 p-4 border-black border-2">
+        <div className = " p-4 row-span-5 grid grid grid-cols-9">
+          <div className="p-4 col-span-3 row-span-5 border-black border-2">
             <label>Army Name:</label>
           </div>
-          <div className = "m-8 p-4 border-black border-2">
+          <div className="p-4 col-span-3 row-span-5 border-black border-2">
             <label>Faction:</label>
           </div>
-          <div className = "m-8 p-4 border-black border-2">
+          <div className="p-4 col-span-3 row-span-5 border-black border-2">
             <label>Total cost:</label>
           </div>
+        </div>
+
           {units}
         </div>
       )
