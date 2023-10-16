6bfa76ba163e64ed1edef599948dd3b23197f089
Jonk <jonathan.ko523@gmail.com>
2023-10-15 20:31:19 -0700

    army columns showing armies. need css

--- src/components/MainPage/MainRight.js
+++ src/components/MainPage/MainRight.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import UnitRow from './UnitRow.js'
+import ArmyColumns from './ArmyColumns.js'
 
 function MainRight({
   army, 
@@ -47,7 +48,6 @@ function MainRight({
 
     return (
         <div className="border-black col-span-10 row-span-6 border-2 grid grid-cols-3">
-          <div className="READIEDARMY col-span-1 border-black border-2">
             <div className="ARMYHEADER">
               <div className="p-4 ">
                 <label>Army Name:</label><input type="text" onChange={handleChangeName}  value={armyName}></input>
@@ -62,8 +62,10 @@ function MainRight({
                 <label>Total cost in money:{totalArmyMoneyCost}</label>
               </div>
             </div>
-            <div className="UNITS CONTAINER">
-              {units}
+            <div className="">
+            <ArmyColumns army={army}/>
+            <ArmyColumns army={army}/>
+            <ArmyColumns army={army}/>
             </div>
             <div className="p-4 col-span-2 row-span-5 border-black border-2">
               <button className = "rounded-md p-2 mt-2 bg-slate-50" type="submit" value="Add Unit" onClick={saveArmyToLocal}>Save Army</button>
@@ -74,7 +76,6 @@ function MainRight({
                 setFaction('')
               }}>Clear Army</button>
             </div>
-          </div>
 
         </div>
       )
