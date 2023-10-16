77cad2afd401de72d2f4115628a421b18dac3836
Jonk <jonathan.ko523@gmail.com>
2023-10-15 20:08:44 -0700

    compartmentalizing zrmy columns

---
+++ src/components/MainPage/ArmyColumns.js
@@ -0,0 +1,64 @@
+import React, {useEffect, useState} from 'react';
+import UnitRow from './UnitRow.js'
+
+function ArmyColumn({army}){
+    //{faction:string, units: object, enhancements:object, pointcost: num, modelcount: num, id: number}
+
+
+
+
+  let units = [];
+  let totalArmyCost = 0;
+    army.units.forEach(cur=>{
+      units.push(<UnitRow 
+        unitName={cur.unitName} 
+        modelCount={cur.modelCount} 
+        pointCost={cur.pointCost} 
+        unitSize={cur.unitSize}
+        unitId={cur.unitId}
+        removeUnit={removeUnit}
+        />)
+    })
+    army.units.forEach(cur=>{
+      totalArmyCost += cur.pointCost
+    })
+
+
+
+
+    return (
+        <div className="border-black col-span-10 row-span-6 border-2 grid grid-cols-3">
+          <div className="READIEDARMY col-span-1 border-black border-2">
+            <div className="ARMYHEADER">
+              <div className="p-4 ">
+                <label>Army Name:</label><input type="text" onChange={handleChangeName}  value={armyName}></input>
+              </div>
+              <div className="p-4">
+                <label>Faction:</label><input type="text"  onChange={handleChangeFaction} value={faction}></input>
+              </div>
+              <div className="p-4  border-black border-2">
+                <label>Total cost in points:{totalArmyCost}</label>
+              </div>
+              <div className="p-4 col-span-2 row-span-6 ">
+                <label>Total cost in money:{totalArmyMoneyCost}</label>
+              </div>
+            </div>
+            <div className="UNITS CONTAINER">
+              {units}
+            </div>
+            <div className="p-4 col-span-2 row-span-5 border-black border-2">
+              <button className = "rounded-md p-2 mt-2 bg-slate-50" type="submit" value="Add Unit" onClick={saveArmyToLocal}>Save Army</button>
+              <button className = "rounded-md p-2 mt-2 bg-slate-50" type="submit" value="Add Unit" onClick={loadArmyFromLocal}>Load Army</button>
+              <button className = "rounded-md p-2 mt-2 bg-slate-50" type="submit" value="Add Unit" onClick={()=>{
+                clearCurrentArmy()
+                setArmyName('')
+                setFaction('')
+              }}>Clear Army</button>
+            </div>
+          </div>
+
+        </div>
+      )
+}
+
+export default ArmyColumn;
