21d9f147d46ec0bee30237ac76c31d56d97707a9
Jonk <jonathan.ko523@gmail.com>
2023-10-21 23:58:13 -0700

    Lot more blueprinting on database

--- src/components/Database/DatabasePage.js
+++ src/components/Database/DatabasePage.js
@@ -10,6 +10,7 @@ function Database({army}){
   //STATE
   //state for current selected army app is using
   //state for armies we pull from database
+  //state for army we clicked on from database 
   
 
   //Function for pulling data on armies from database, showing it on screen
@@ -19,11 +20,21 @@ function Database({army}){
     //Armies altered in app will not be updating database in realtime
   //Function for deleting an army from database
 
-  
+  //needs a delete form local button passed down from the APP
 
+  //Database Left
+    //component displaying current army being edited in the app
+    //save button for said component
+    //delete from local button
 
-  //function 
-
+  //Database Right
+      //component for displaying all armies in database.
+      //ability to click on an army to select if(checkbox-like. needs state)
+      //ability to click on multiple?
+      //button to delete said database item
+      //button to load said database item(set to app state)
+      //button to copy said database item(create a copy of it in database)
+      //ability to rearrage. drag and drop is ideal
 
 
 
