d13b43deef402714aa3b2d377c8b3c085bba50c7
Jonk <jonathan.ko523@gmail.com>
2023-10-23 21:36:16 -0700

    transition is starting out rough

---
+++ app/pages.jsx
@@ -0,0 +1,18 @@
+/**
+
+Renders a Next.js page component that displays a grid of character avatars with links to individual character pages.
+@component
+@returns {JSX.Element} The rendered page component.
+*/
+
+import { App } from '@/components'
+
+
+export default async function Page() {
+
+  return (
+    <main>
+      <App />
+    </main>
+  )
+}
