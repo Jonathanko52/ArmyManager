61a72843177a2ce19805b1f424f0561c546c0fb1
Jonk <jonathan.ko523@gmail.com>
2023-10-23 22:24:56 -0700

    next was not ignored. Have it working without css thought

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import App from '../src/App'
+  
+  export default function HomePage() {
+
+  
+    return (
+      <div>
+        <App/>
+      </div>
+    );
+  }
