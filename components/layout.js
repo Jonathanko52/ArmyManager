90e9675970748a6a0caa86f77ec8aa0622c2b708
Jonk <jonathan.ko523@gmail.com>
2023-10-24 18:23:44 -0700

    remove previously tracked

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,11 @@
 import NavBar from './navBar'
+import Header from './Header'
 
 
 export default function Layout({ children }) {
     return (
     <div className="App grid grid-cols-8 h-screen	bg-slate-500 text-gray-300">
+        <Header/>
         <NavBar/>
         {children}
     </div>
