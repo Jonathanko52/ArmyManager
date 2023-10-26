0af4a8a1f662802b582e0e8818526c0f48204834
Jonk <jonathan.ko523@gmail.com>
2023-10-25 23:37:46 -0700

    testing prop sharing in the layout component

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,8 +1,13 @@
 import NavBar from './navBar'
 import Header from './Header'
+import React, {useState} from 'react';
 
 
 export default function Layout({ children }) {
+
+    const [armyReady, setArmyReady] = useState("TEST");
+    console.log(armyReady, children)
+
     return (
     <div className="App grid grid-cols-8 h-screen bg-slate-500 text-gray-300">
         <Header/>
