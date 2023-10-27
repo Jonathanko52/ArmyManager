5c9b74791e16cc84dd9bd4043ffa933b09375e3b
Jonk <jonathan.ko523@gmail.com>
2023-10-26 21:17:23 -0700

    can't transfer state using layout. Kinda as expected

--- pages/Database.jsx
+++ pages/Database.jsx
@@ -2,11 +2,11 @@ import Layout from '../components/layout'
 import Database from '../components/Database/DatabasePage'
 
 
-export default function Database() {  
+export default function DatabasePage() {  
     return (
         <Layout>
+            DATABASE
             <Database/>
         </Layout>
-
     );
   }
