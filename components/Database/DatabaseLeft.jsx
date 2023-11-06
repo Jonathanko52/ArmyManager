85d790f7efe4b337079219c504ffed2959aa7efe
Jonk <jonathan.ko523@gmail.com>
2023-11-05 22:04:59 -0800

    Added displaying database armies

--- components/Database/DatabaseLeft.jsx
+++ components/Database/DatabaseLeft.jsx
@@ -16,10 +16,9 @@ function DatabaseLeft({
   logCurrentState
 }){
 
-
     return (
         <div className="p-2 m-2 border-black col-span-4 row-span-5 border-1">
-          <button className = "rounded-md p-2 m-2 bg-slate-50 text-black" type="submit" value="Add Unit" onClick={()=>{addArmy()}}>Add Army</button>
+          <button className = "rounded-md p-2 m-2 bg-slate-50 text-black" type="submit" value="Add Unit" onClick={()=>{addArmy()}}>Add Army to Database</button>
           <button className = "rounded-md p-2 m-2 bg-slate-50 text-black" type="submit" value="Add Unit" onClick={()=>{createTable()}}>Create Table</button>
           <button className = "rounded-md p-2 m-2 bg-slate-50 text-black" type="submit" value="Add Unit" onClick={()=>{deleteArmy()}}>Delete Army</button>
           <button className = "rounded-md p-2 m-2 bg-slate-50 text-black" type="submit" value="Add Unit" onClick={()=>{retrieveArmy()}}>Retrieve Army</button>
--- components/Database/DatabaseRight.jsx
+++ components/Database/DatabaseRight.jsx
@@ -11,24 +11,21 @@ function DatabaseRight({databaseArmies}){
     if(databaseArmies){
       databaseArmies.forEach((cur)=>{
         console.log("DATABASE CURRENt", JSON.parse(cur.armyString.S))
-        // <ArmyRow 
-        //   armyName={cur.armyName.S}
-        //   armyfaction={cur.armyFaction.S}
-        //  armyPoints=={cur.armyPoints.S}
-        // />
+        let parsedArmy = JSON.parse(cur.armyString.S)
+        let armyName = parsedArmy.armyName
+        let armyFaction = parsedArmy.faction
+        let armyPoints = parsedArmy.armyPoints
+        armies.push(<ArmyRow 
+          armyName={armyName}
+          armyfaction={armyFaction}
+         armyPoints={armyPoints}
+        />)
       })
     }
-      //   {
-      //     armyString: {
-      //       S: `{"armyName":"Black Templars","faction":"Black Templars","armyReady":{"units":[],"armyName":"Black Templar","faction":"Black Templar"},"armyStandby":{"units":[{"unitName":"Crusader Squad","modelCount":10,"pointCost":140,"unitSize":0,"moneyCost":0,"unitId":942},{"unitName":"Castellan","modelCount":1,"pointCost":65,"unitSize":0,"moneyCost":0,"unitId":467},{"unitName":"Emperor's Champion","modelCount":1,"pointCost":75,"unitSize":0,"moneyCost":0,"unitId":386}]},"armyUnpainted":{"units":[{"unitName":"Crusader Squad","modelCount":10,"pointCost":140,"unitSize":0,"moneyCost":0,"unitId":982}]}}`
-      //     },
-      //     armyName: { S: 'Black Templars' },
-      //     armyFaction: { S: 'Black Templars' }
-      //   }
 
     return (
         <div className="font-bold underline border-black col-span-8 row-span-5 border-2">
-          {/* {armies} */}
+          {armies}
         </div>
       )
 }
