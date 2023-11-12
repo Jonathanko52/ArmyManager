c45e0237a7076eec72f8420d3314839cb0bc2e3f
Jonk <jonathan.ko523@gmail.com>
2023-11-11 21:44:08 -0800

    chaning button order

--- components/ArmyPage/UnitRow.jsx
+++ components/ArmyPage/UnitRow.jsx
@@ -21,8 +21,7 @@ function UnitRow({unitName, modelCount, pointCost, unitId, buttonOne,
           <label className="m-4 p-4" >Model Count: {modelCount}</label><br></br>
           <label className="m-4 p-4" >Point Cost: {pointCost}</label><br></br>
           <label className="m-4 p-4" >Unit ID: {unitId}</label><br></br>
-          <button className="rounded-md p-1 m-1 bg-slate-50 text-black" onClick={()=>{buttonThree(unitId)}}>Move Up</button>
-          <button className="rounded-md p-1 m-1 bg-slate-50 text-black" onClick={()=>{buttonFour(unitId)}}>Move Down</button>
+
           <div className="p-4 ">
               <button className="rounded-md p-2 m-2 bg-slate-50 text-black" type="submit" value="Add Unit" 
               onClick={()=>{buttonOne(unitId)}}>{buttonOneText}</button>
@@ -32,6 +31,10 @@ function UnitRow({unitName, modelCount, pointCost, unitId, buttonOne,
               onClick={()=>{remove(unitId)}}>Remove</button>
               <button className="rounded-md p-2 m-2 bg-slate-50 text-black" type="submit" value="Add Unit" 
               onClick={()=>{duplicate(unitId)}}>Duplicate</button>
+              <button className="rounded-md p-1 m-1 bg-slate-50 text-black" 
+              onClick={()=>{buttonThree(unitId)}}>Move Up</button>
+              <button className="rounded-md p-1 m-1 bg-slate-50 text-black" 
+              onClick={()=>{buttonFour(unitId)}}>Move Down</button>
             </div>
         </div>
       )
