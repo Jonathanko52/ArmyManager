784923f8f33b8e0b37febaf816ba0d3ce1315045
Jonk <jonathan.ko523@gmail.com>
2023-11-13 13:47:41 -0800

    fixed retrieve army. Api calls are horrible

--- pages/api/retrieveArmy.js
+++ pages/api/retrieveArmy.js
@@ -1,19 +1,22 @@
 import AWSConfig from './config/aws-config'
 const AWS = require("aws-sdk");
 const dynamodb = new AWS.DynamoDB();
+AWS.config.update(AWSConfig);
+
 
 
 export default function retrieveArmy(req, res) {
   return new Promise((resolve,reject)=>{
-    AWS.config.update({region: "us-west-1"});
     const params = {
       TableName: "WarhammerArmies",
     };
+    AWS.config.update({region: 'us-west-1'});
+
     dynamodb.scan(params, function(err, data) {
       if (err) {
-        console.error("Unable to find movie", err);
+        console.error("Unable to find army", err);
       } else {
-        console.log("Found movie", data.Items);
+        console.log("Found armies");
         res.status(400).send(data.Items)
       }
     });
