abe48ff9ba936f6399f8c128e6934d5cf4ab486d
Jonk <jonathan.ko523@gmail.com>
2023-12-04 17:09:34 -0800

    black templar points added

--- data/blackTemplayPoints.js
+++ data/blackTemplayPoints.js
@@ -1,31 +1,24 @@
 module.exports = {
-    Black Templars Gladiator Lancer:{models:[["1 model", 165]],unitLimit:3},
-    Black Templars Gladiator Reaper:{models:[["1 model", 155]],unitLimit:3},
-    Black Templars Gladiator Valiant:{models:[["1 model", 155]],unitLimit:3},
-    Black Templars Impulsor:{models:[["1 model", 85]],unitLimit:3},
-    Black Templars Repulsor:{models:[["1 model", 200]],unitLimit:3},
-    Black Templars Repulsor Executioner:{models:[["1 model", 235]],unitLimit:3},
+    BlackTemplarsGladiatorLancer:{models:[["1 model", 165]],unitLimit:3},
+    BlackTemplarsGladiatorReaper:{models:[["1 model", 155]],unitLimit:3},
+    BlackTemplarsGladiatorValiant:{models:[["1 model", 155]],unitLimit:3},
+    BlackTemplarsImpulsor:{models:[["1 model", 85]],unitLimit:3},
+    BlackTemplarsRepulsor:{models:[["1 model", 200]],unitLimit:3},
+    BlackTemplarsRepulsorExecutioner:{models:[["1 model", 235]],unitLimit:3},
     Castellan:{models:[["1 model", 65]],unitLimit:3},
-    Chaplain Grimaldus:{models:[["4 model", 95]],unitLimit:1},
-    Crusader Squad:{models:[["1 model", 185]],unitLimit:3},
-    1 Sword Brother and 4 Initiates  65 pts
-    1 Sword Brother and 9 Initiates 125 pts
-    1 Sword Brother, 4 Initiates
-    and 5 Neophytes 125 pts
-    1 Sword Brother, 9 Initiates
-    and 10 Neophytes 250 pts
-    High Marshal Helbrecht:{models:[["1 model", 185]],unitLimit:3},
-    1 model  95 pts
-    Marshal:{models:[["1 model", 185]],unitLimit:3},
-    1 model  80 pts
-    Primaris Crusader Squad:{models:[["1 model", 185]],unitLimit:3},
-    1 Sword Brother, 5 Initiates
-    and 4 Neophytes 140 pts
-    1 Sword Brother, 11 Initiates
-    and 8 Neophytes 280 pts
-    Primaris Sword Brethren:{models:[["1 model", 185]],unitLimit:3},
-    5 models 140 pts
-    10 models 280 pts
-    The Emperor’s Champion:{models:[["1 model", 185]],unitLimit:3},
-    1 model  75 pts
+    ChaplainGrimaldus:{models:[["4 model", 95]],unitLimit:1},
+    CrusaderSquad:{models:[
+        ["1 Sword Brother and 4 Initiates", 65],
+        ["1 Sword Brother and 9 Initiates", 125],
+        ["1 Sword Brother, 4 Initiates and 5 Neophytes", 125],
+        ["1 Sword Brother, 9 Initiates and 10 Neophytes", 250]
+    ],unitLimit:3},
+    HighMarshalHelbrecht:{models:[["1 model", 95]],unitLimit:1},
+    Marshal:{models:[["1 model", 80]],unitLimit:3},
+    PrimarisCrusaderSquad:{models:[
+        ["1 Sword Brother, 5 Initiates and 4 Neophytes", 140],
+        ["1 Sword Brother, 11 Initiates and 8 Neophytes", 280],
+    ],unitLimit:3},
+    PrimarisSwordBrethren:{models:[["5 models", 140],["10 models", 280]],unitLimit:3},
+    TheEmperorsChampion:{models:[["1 model", 75]],unitLimit:1},
 }
