569cb435976dc0927d459beeef208005cbe6b62e
Jonk <jonathan.ko523@gmail.com>
2023-12-11 21:22:46 -0800

    fixed datbase left

--- components/Database/DatabaseLeft.jsx
+++ components/Database/DatabaseLeft.jsx
@@ -18,15 +18,15 @@ function DatabaseLeft({
 
     return (
         <div className="p-2 m-2 border-black col-span-4 row-span-5 border-1">
-        <h1 className="p-4 text-lg font-bold">Current Army</h1>
-        <ArmyRow
-          armyName={armyName}
-          armyfaction={armyFaction}
-          armyPoints={armyPoints}
-          buttonOne={addArmy}
-          buttonOneText={"Save To Database"}
-          buttonOneParam={armyName}
-        />
+          <h1 className="p-4 text-lg font-bold">Current Army</h1>
+            <ArmyRow
+              armyName={armyName}
+              armyfaction={armyFaction}
+              armyPoints={armyPoints}
+              buttonOne={addArmy}
+              buttonOneText={"Save To Database"}
+              buttonOneParam={armyName}
+            />
           <button className = "rounded-md p-2 pl-4 pr-4 m-2 bg-slate-50 hover:bg-slate-200 text-black font-bold" type="submit" value="Add Unit" onClick={()=>{createTable()}}>Create Table</button>
           <button className = "rounded-md p-2 pl-4 pr-4 m-2 bg-slate-50 hover:bg-slate-200 text-black font-bold" type="submit" value="Add Unit" onClick={()=>{retrieveArmy()}}>Retrieve Army</button>
           <button className = "rounded-md p-2 pl-4 pr-4 m-2 bg-slate-50 hover:bg-slate-200 text-black font-bold" type="submit" value="Add Unit" onClick={()=>{updateArmy()}}>Update Army</button>
