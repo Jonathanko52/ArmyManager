47fdf30783c74208bbba62218204e3e7b9e9db0e
Jonk <jonathan.ko523@gmail.com>
2023-12-14 21:47:03 -0800

    army summary component created

---
+++ components/ArmyPage/ArmySummary.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ArmySummary({
+    //props
+}){
+    return (
+        <div 
+          className="border-black col-span-8 row-span-2 border-2 p-4 m-4 rounded-md grid grid-cols-4" 
+        >
+        </div>
+      )
+}
+
+export default ArmySummary;
