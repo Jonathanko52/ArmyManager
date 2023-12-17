5853d19b53b8b1558243f88380561fd8a5ec2665
Jonk <jonathan.ko523@gmail.com>
2023-12-16 22:10:42 -0800

    rough summary printing

--- components/ArmyPage/ArmySummary.jsx
+++ components/ArmyPage/ArmySummary.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
-function ArmySummary({
-    //props
-}){
+function ArmySummary({armyReady}){
+    console.log(armyReady)
+    let result = []
+    armyReady.units.forEach((cur,ind)=>{
+        console.log(cur.unitName)
+        result.push(<li>{cur.unitName}</li>)
+    })
     return (
-        <div 
-          className="border-black col-span-8 row-span-2 border-2 p-4 m-4 rounded-md grid grid-cols-4"  >
+        <div className="border-black col-span-8 row-span-2 border-2 p-4 m-4 rounded-md grid grid-cols-4">
+            <ul>
+                {result}
+            </ul>
         </div>
       )
 }
