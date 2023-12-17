a38d35233cb336f58cce798db686679f549d0a7c
Jonk <jonathan.ko523@gmail.com>
2023-12-16 22:12:17 -0800

    refining

--- components/ArmyPage/ArmySummary.jsx
+++ components/ArmyPage/ArmySummary.jsx
@@ -3,10 +3,14 @@ import React from 'react';
 function ArmySummary({armyReady}){
     console.log(armyReady)
     let result = []
+    let convertedObj = armyReady.units.reduce()
     armyReady.units.forEach((cur,ind)=>{
         console.log(cur.unitName)
         result.push(<li>{cur.unitName}</li>)
     })
+
+
+
     return (
         <div className="border-black col-span-8 row-span-2 border-2 p-4 m-4 rounded-md grid grid-cols-4">
             <ul>
